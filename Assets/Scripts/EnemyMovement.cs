04e14744e43bd287c885752d5930b69ed8d49a13
Cha Seoyeon <seoyeon0833@gmail.com>
2024-02-23 03:06:16 +0900

    적 오브젝트 및 스크립트 추가

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float moveSpeed = 5f;
+
+    Rigidbody2D rigid;
+    Animator anim;
+
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        Jump();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float dir = target.position.x - transform.position.x;
+
+        if (Mathf.Abs(dir) < 8)
+        {
+            Vector2 nextPos = new Vector2(Mathf.Sign(dir) * Mathf.Abs(moveSpeed), 0f);
+            rigid.velocity = nextPos;
+
+            transform.localScale = new Vector3(Mathf.Sign(dir), 1, 1);
+        }
+        else
+        {
+            rigid.velocity = new Vector2(moveSpeed, 0f);
+            transform.localScale = new Vector3(Mathf.Sign(moveSpeed), 1, 1);
+        }
+    }
+
+    void Jump()
+    {
+        anim.SetBool("IsJumping", true);
+        Invoke("EndJump", 0.5f);
+    }
+
+    void EndJump()
+    {
+        anim.SetBool("IsJumping", false);
+        float ranNum = Random.Range(2f, 6f);
+        Invoke("Jump", ranNum);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        moveSpeed = -moveSpeed;
+        transform.localScale = new Vector3(Mathf.Sign(moveSpeed), 1, 1);
+    }
+}
